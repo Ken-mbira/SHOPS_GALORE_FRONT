279c3b79e0f4b7730d01199f1755d1452fd46d2d
ken <ken.mbira@student.moringaschool.com>
2021-12-13 15:34:17 +0300

    add category component

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { CategoryComponent } from './landing/category/category.component';
+import { SliderComponent } from './landing/slider/slider.component';
 
 
 @NgModule({
   declarations: [
     CustomersComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingComponent,
+    CategoryComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
