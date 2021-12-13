97c32cd81b7c1e81ea82d7765cea09674d887e24
ken <ken.mbira@student.moringaschool.com>
2021-12-13 10:44:56 +0300

    make smooth transitions between pages

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -4,11 +4,22 @@ import { AuthenticationComponent } from './authentication.component';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RoleChoiceComponent } from './register/role-choice/role-choice.component';
+import { FormComponent } from './register/form/form.component';
+import { SuccessfullComponent } from './register/successfull/successfull.component';
 
 const routes: Routes = [
   { path: '', component: AuthenticationComponent },
   {path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent },
+  { 
+    path: 'register', 
+    component: RegisterComponent,
+    children: [
+      {path: '', component:RoleChoiceComponent},
+      {path: 'form', component:FormComponent},
+      {path: 'success', component:SuccessfullComponent}
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,13 +5,19 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RoleChoiceComponent } from './register/role-choice/role-choice.component';
+import { SuccessfullComponent } from './register/successfull/successfull.component';
+import { FormComponent } from './register/form/form.component';
 
 
 @NgModule({
   declarations: [
     AuthenticationComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    RoleChoiceComponent,
+    SuccessfullComponent,
+    FormComponent
   ],
   imports: [
     CommonModule,
