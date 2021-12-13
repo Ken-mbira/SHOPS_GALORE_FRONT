02bda615f1777d902670645e757de2cf47e1dd73
ken <ken.mbira@student.moringaschool.com>
2021-12-13 06:54:05 +0300

    add not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
   {
@@ -12,7 +14,8 @@ const routes: Routes = [
   { path: 'seller', loadChildren: () => import('./seller/seller.module').then(m => m.SellerModule) },
   { path: 'delivery', loadChildren: () => import('./delivery/delivery.module').then(m => m.DeliveryModule) },
   { path: 'staff', loadChildren: () => import('./staff/staff.module').then(m => m.StaffModule) },
-  { path: 'buyer', loadChildren: () => import('./buyer/buyer.module').then(m => m.BuyerModule) }
+  { path: 'buyer', loadChildren: () => import('./buyer/buyer.module').then(m => m.BuyerModule) },
+  { path: '**', component: NotFoundComponent},
 
 ];
 
