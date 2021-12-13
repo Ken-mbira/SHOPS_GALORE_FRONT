8b37e39fd1846c03e6b594977b55b73cc664ac81
ken <ken.mbira@student.moringaschool.com>
2021-12-13 06:49:38 +0300

    add all needed modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,12 @@ const routes: Routes = [
     path: '',
     redirectTo: '',
     pathMatch: 'full'
-  }
+  },
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
+  { path: 'seller', loadChildren: () => import('./seller/seller.module').then(m => m.SellerModule) },
+  { path: 'delivery', loadChildren: () => import('./delivery/delivery.module').then(m => m.DeliveryModule) },
+  { path: 'staff', loadChildren: () => import('./staff/staff.module').then(m => m.StaffModule) },
+  { path: 'buyer', loadChildren: () => import('./buyer/buyer.module').then(m => m.BuyerModule) }
 
 ];
 
