4ed636f13a32817862e147573f45447eacf40f89
ken <ken.mbira@student.moringaschool.com>
2021-12-18 11:08:25 +0300

    modify the sidenav

--- src/app/delivery/delivery-routing.module.ts
+++ src/app/delivery/delivery-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DeliveryComponent } from './delivery.component';
 
-const routes: Routes = [{ path: '', component: DeliveryComponent }];
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DeliveryComponent,
+    children: [
+      { path:'', component: DashboardComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/delivery/delivery.module.ts
+++ src/app/delivery/delivery.module.ts
@@ -5,7 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider'; 
+import { MatDividerModule } from '@angular/material/divider';
 
 import { DeliveryRoutingModule } from './delivery-routing.module';
 import { DeliveryComponent } from './delivery.component';
