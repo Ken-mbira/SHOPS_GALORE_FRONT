dd3bbaaead565aadfc45eeb84f0b06ea67107f8b
ken <ken.mbira@student.moringaschool.com>
2021-12-18 18:13:09 +0300

    add all router links for the delivery module

--- src/app/delivery/delivery-routing.module.ts
+++ src/app/delivery/delivery-routing.module.ts
@@ -3,13 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { DeliveryComponent } from './delivery.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DeliveriesComponent } from './deliveries/deliveries.component';
+import { NotificationComponent } from './notification/notification.component';
+import { MeansComponent } from './means/means.component';
+import { DestinationComponent } from './destination/destination.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard'},
   {
     path: '',
     component: DeliveryComponent,
     children: [
-      { path:'', component: DashboardComponent }
+      { path:'dashboard', component: DashboardComponent },
+      {path: 'deliveries', component:DeliveriesComponent},
+      {path: 'notification', component: NotificationComponent},
+      {path: 'means', component: MeansComponent},
+      {path: 'destination', component:DestinationComponent}
     ]
   },
 ];
