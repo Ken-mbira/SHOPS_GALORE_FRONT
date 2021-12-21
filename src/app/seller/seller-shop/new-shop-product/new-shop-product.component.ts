162889588edf1041e0bff629fe526fc591241e22
ken <ken.mbira@student.moringaschool.com>
2021-12-21 12:22:20 +0300

    add router link to new product page

---
+++ src/app/seller/seller-shop/new-shop-product/new-shop-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-shop-product',
+  templateUrl: './new-shop-product.component.html',
+  styleUrls: ['./new-shop-product.component.css']
+})
+export class NewShopProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
