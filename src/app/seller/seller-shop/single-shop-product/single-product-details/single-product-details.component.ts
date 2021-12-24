0669b66e9d64975ac6344acad383ada34e0360c4
ken <ken.mbira@student.moringaschool.com>
2021-12-24 16:21:43 +0300

    add drag and drop upload

--- src/app/seller/seller-shop/single-shop-product/single-product-details/single-product-details.component.ts
+++ src/app/seller/seller-shop/single-shop-product/single-product-details/single-product-details.component.ts
@@ -9,6 +9,22 @@ export class SingleProductDetailsComponent implements OnInit {
 
   constructor() { }
 
+  images:any = [
+    {src : "../../../../../assets/andrew-pons-cLHPacdtpSY-unsplash (1).jpg", isDefault:false},
+    {src : "../../../../../assets/andrew-pons-cLHPacdtpSY-unsplash (1).jpg", isDefault:false}
+  ]
+
+  fileBrowseHandler(event){
+    console.log(event.files[0])
+    var reader = new FileReader();
+    reader.readAsDataURL(event.files[0])
+
+    reader.onload = (_event) => {
+      var image = {src : reader.result,isDefault: false}
+      this.images.push(image)
+    }
+  }
+
   ngOnInit(): void {
   }
 
