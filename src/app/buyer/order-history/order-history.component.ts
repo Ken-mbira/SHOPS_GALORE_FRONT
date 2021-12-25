2e4af8f655bf2a1a029e6fd1c2701320eb3d7e7c
ken <ken.mbira@student.moringaschool.com>
2021-12-25 11:06:39 +0300

    add specific order view

--- src/app/buyer/order-history/order-history.component.ts
+++ src/app/buyer/order-history/order-history.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Order{
+export interface Orders{
   id:number;
   date:Date;
   items:number;
   price:string;
 }
 
-const ORDER_DATA:Order[] = [
+const ORDER_DATA:Orders[] = [
   { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
-  { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
-  { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
-  { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
-  { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
-  { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
-  { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
+  { id:2, date:new Date(12/12/2112),items:23,price:"2,1212" },
+  { id:3, date:new Date(12/12/2112),items:23,price:"2,1212" },
+  { id:4, date:new Date(12/12/2112),items:23,price:"2,1212" },
+  { id:5, date:new Date(12/12/2112),items:23,price:"2,1212" },
+  { id:6, date:new Date(12/12/2112),items:23,price:"2,1212" },
+  { id:7, date:new Date(12/12/2112),items:23,price:"2,1212" },
   { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
   { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
   { id:1, date:new Date(12/12/2112),items:23,price:"2,1212" },
--- src/app/buyer/single-order/single-order.component.ts
+++ src/app/buyer/single-order/single-order.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Order{
+  id:number;
+  product_name:string;
+  quantity:number;
+  price:string;
+}
+
+const ORDER_ITEMS:Order[] = [
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+  {id:1,product_name:"Handkerchief",quantity:3,price:"2,121"},
+]
+
 @Component({
   selector: 'app-single-order',
   templateUrl: './single-order.component.html',
   styleUrls: ['./single-order.component.css']
 })
 export class SingleOrderComponent implements OnInit {
+
+  displayedColumns: string[] = ['id','name','quantity','price']
+
+  dataSource = ORDER_ITEMS;
 
   constructor() { }
 
