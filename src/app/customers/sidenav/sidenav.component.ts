ce5d776e1bddd0f64b5f909f6861a8acda7418d8
ken <ken.mbira@student.moringaschool.com>
2021-12-26 17:32:47 +0300

    transfer nested category tree to the category component

--- src/app/customers/sidenav/sidenav.component.ts
+++ src/app/customers/sidenav/sidenav.component.ts
@@ -1,35 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NestedTreeControl } from '@angular/cdk/tree';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
-
-interface Category{
-  name: string;
-  id:number;
-  children? : Category[];
-}
-
-const TREE_DATA: Category[] = [
-  { 
-    name:"Clothing", id:1, children:[
-      { name:"Men's Clothing", id:2},
-      { name:"Women's Clothing", id:3,children:[
-        { name:"Men's Clothing", id:2},
-        { name:"Men's Clothing", id:2, children:[
-          { name:"Men's Clothing", id:2},
-          { name:"Men's Clothing", id:2},
-        ]},
-      ]
-    }
-    ]
-  },
-  { name:"Electronics", id:4, children:[
-      {name:"Speakers", id:5},
-      {name:"Television", id:6},
-    ]
-  }
-]
-
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -37,21 +7,7 @@ const TREE_DATA: Category[] = [
 })
 export class SidenavComponent implements OnInit {
 
-  treeControl = new NestedTreeControl<Category>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<Category>()
-
-  constructor() {
-    this.dataSource.data = TREE_DATA
-  }
-
-  toggle:boolean = false;
-
-  changeToggle(){
-    this.toggle = !this.toggle
-  }
-
-  hasChild = (_: number, node: Category) => !!node.children && node.children.length > 0;
-
+  constructor() { }
   ngOnInit(): void {
   }
 
