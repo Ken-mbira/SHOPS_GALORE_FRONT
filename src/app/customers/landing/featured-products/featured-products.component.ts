a870bba52cc7f9a36d2ca13e4abaa71c3dfb5d4a
ken <ken.mbira@student.moringaschool.com>
2021-12-27 15:25:02 +0300

    add featured products carousel

--- src/app/customers/landing/featured-products/featured-products.component.ts
+++ src/app/customers/landing/featured-products/featured-products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 @Component({
   selector: 'app-featured-products',
   templateUrl: './featured-products.component.html',
@@ -8,6 +10,44 @@ import { Component, OnInit } from '@angular/core';
 export class FeaturedProductsComponent implements OnInit {
 
   constructor() { }
+
+  customOptions: OwlOptions = {
+    pullDrag: true,
+    merge: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    margin: 20,
+    autoplayMouseleaveTimeout: 2000,
+    dots: true,
+    navSpeed: true,
+    autoplaySpeed: 3000,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      500:{
+        items:2
+      },
+      600:{
+        items:3
+      },
+      740: {
+        items: 4
+      },
+      1000: {
+        items: 5
+      }
+    },
+    nav: false
+  }
 
   ngOnInit(): void {
   }
