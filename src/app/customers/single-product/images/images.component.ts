acb302f6e63c276460e435138405b07e2a50ade4
ken <ken.mbira@student.moringaschool.com>
2021-12-27 16:31:14 +0300

    add image slider on product view

--- src/app/customers/single-product/images/images.component.ts
+++ src/app/customers/single-product/images/images.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
@@ -8,6 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class ImagesComponent implements OnInit {
 
   constructor() { }
+
+  customOptions: OwlOptions = {
+    pullDrag: true,
+    merge: true,
+    autoplay: false,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    margin: 20,
+    autoplayMouseleaveTimeout: 2000,
+    dots: true,
+    navSpeed: true,
+    autoplaySpeed: 3000,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 4
+      }
+    },
+    nav: false
+  }
 
   ngOnInit(): void {
   }
