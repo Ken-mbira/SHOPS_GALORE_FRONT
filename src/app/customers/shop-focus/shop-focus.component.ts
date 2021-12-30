e84de1c720ce44ebee9b47b12202c2bf9b1e633c
ken <ken.mbira@student.moringaschool.com>
2021-12-30 15:38:29 +0300

    add filter and main content to the shop page

--- src/app/customers/shop-focus/shop-focus.component.ts
+++ src/app/customers/shop-focus/shop-focus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-shop-focus',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopFocusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private observer:BreakpointObserver) { }
+
+  belowWidth:boolean = false;
+
+  ngAfterViewInit():void{
+
+    this.observer.observe(['(max-width:900px)']).subscribe((res) => {
+      if(res.matches){
+        this.belowWidth = true;
+      }else{
+        this.belowWidth = false;
+      }
+    })
+
+  }
 
   ngOnInit(): void {
   }
