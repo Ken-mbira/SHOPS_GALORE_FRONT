153d4290f200c0ca120ef29223cbcca9b96c8c50
ken <ken.mbira@student.moringaschool.com>
2022-01-22 20:43:07 +0300

    provide the service to the authentication module

--- src/app/authentication/password/password.component.ts
+++ src/app/authentication/password/password.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { BreakpointObserver } from '@angular/cdk/layout';
 import {StepperOrientation} from '@angular/material/stepper';
+import { MatStepper } from '@angular/material/stepper';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
-import { MatStepper } from '@angular/material/stepper';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { PasswordService } from 'src/app/services/Passwords/password.service';
 
 @Component({
   selector: 'app-password',
@@ -19,10 +21,20 @@ export class PasswordComponent implements OnInit {
 
   stepperOrientation: Observable<StepperOrientation>;
 
-  constructor(breakpointObserver: BreakpointObserver,private fb:FormBuilder,private snackBar:MatSnackBar) {
+  constructor(breakpointObserver: BreakpointObserver,private fb:FormBuilder,private snackBar:MatSnackBar,private passwordService:PasswordService, private router:Router) {
     this.stepperOrientation = breakpointObserver
       .observe('(min-width: 500px)')
       .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+  }
+
+  resetPassword(event:FormGroup){
+    this.passwordService.resetPassword(event).subscribe(response => {
+      this.snackBar.open("The password was successfully reset","Log in",{duration:3000})
+      this.router.navigate(['auth/login']);
+    },error => {
+      console.log(error)
+      this.snackBar.open("There was a problem resetting the password","Sorry",{duration:3000})
+    })
   }
 
   ngOnInit(): void {
--- src/app/authentication/password/reset/reset.component.ts
+++ src/app/authentication/password/reset/reset.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder,FormControl,FormGroup, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+
+import { PasswordService } from 'src/app/services/Passwords/password.service';
 
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
   styleUrls: ['./reset.component.css']
 })
+
 export class ResetComponent implements OnInit {
 
-  constructor() { }
+  resetForm = this.fb.group({
+    token : ['',Validators.required],
+    password: ['', [Validators.required]]
+  })
+
+  @Output() formReset = new EventEmitter<FormGroup>()
+
+  constructor(private fb:FormBuilder) { }
+
+  sendOutForm(){
+    this.formReset.emit(this.resetForm)
+  }
 
   ngOnInit(): void {
   }
