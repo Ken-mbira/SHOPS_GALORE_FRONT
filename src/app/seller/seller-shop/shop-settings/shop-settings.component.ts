7033120d926378bc584e753d0ad0ca8636f6cf25
ken <ken.mbira@student.moringaschool.com>
2022-03-10 10:43:48 +0300

    create profile form

--- src/app/seller/seller-shop/shop-settings/shop-settings.component.ts
+++ src/app/seller/seller-shop/shop-settings/shop-settings.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialog } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { ImageUploaderComponent } from 'src/app/shared_components/image-uploader/image-uploader.component';
+import { validatePhoneNumber } from 'src/app/seller/seller-main/new-shop/new-shop.component';
 
 @Component({
   selector: 'app-shop-settings',
@@ -10,13 +12,22 @@ import { ImageUploaderComponent } from 'src/app/shared_components/image-uploader
 })
 export class ShopSettingsComponent implements OnInit {
 
+
   enterImage():void{
     const dialogRef = this.imageDialog.open(ImageUploaderComponent,{
       width:'250px'
     });
     dialogRef.afterClosed().subscribe((result) => console.log(result))
   }
-  constructor(public imageDialog:MatDialog) { }
+  constructor(public imageDialog:MatDialog,private fb:FormBuilder) { }
+
+  profileForm = this.fb.group({
+    name:['',Validators.required],
+    bio:['',Validators.required],
+    phone_contact:['',[Validators.required,Validators.minLength(9),Validators.maxLength(9),validatePhoneNumber()]],
+    email_contact:['',[Validators.required,Validators.email]],
+    pickup_location:[""]
+  })
   ngOnInit(): void {
   }
 
